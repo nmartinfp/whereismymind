b6af97e4e845a497c5c7eb6b890dfcd57fc5dc9b
Nelson Pereira <nmfp@mail.com>
2017-07-13 19:37:58 +0100

    Database connection working

--- src/main/java/org/academiadecodigo/mindblowers/server/ConnectionManager.java
+++ src/main/java/org/academiadecodigo/mindblowers/server/ConnectionManager.java
@@ -1,5 +1,9 @@
 package org.academiadecodigo.mindblowers.server;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Developed @ <Academia de Código_>
  * Created by
@@ -7,4 +11,38 @@ package org.academiadecodigo.mindblowers.server;
  */
 
 public class ConnectionManager {
+
+    Connection connection = null;
+
+
+    public Connection getConnection() {
+
+        if (connection == null) {
+
+            try {
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mymind", "root", "");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Failure to connect to database : " + e.getMessage());
+                System.exit(1);
+            }
+        }
+        return connection;
+
+    }
+
+
+    public void close() {
+
+        if (connection != null) {
+
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Failure to close database connections: " + e.getMessage());
+                System.exit(1);
+            }
+        }
+    }
 }
